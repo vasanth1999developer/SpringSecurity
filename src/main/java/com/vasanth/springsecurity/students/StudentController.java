1bf9d7b518881cee45dca7a59faea2129d20de8d
Vasanth S <vasumalar1920@gmail.com>
2026-07-05 00:29:19 +0530

    daily learnings-CSRF

--- src/main/java/com/vasanth/springsecurity/students/StudentController.java
+++ src/main/java/com/vasanth/springsecurity/students/StudentController.java
@@ -1,11 +1,11 @@
 package com.vasanth.springsecurity.students;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,24 +14,51 @@ import java.util.List;
 public class StudentController {
 
 
-    private static final List<Student> StudentsList = Arrays.asList(
+
+
+    private List<Student> studentsList = new ArrayList<>(Arrays.asList(
             new Student(1,"vasanth"),
             new Student(2,"rahul"),
-            new Student(3,"vijay")
-
-    );
-
-
+            new Student(3,"vijay"),
+            new Student(4,"vasu"),
+            new Student(5,"jeeva"),
+            new Student(6,"soundar")
+    ));
 
     @GetMapping(path = "{studentId}")
-    public Student getStudent(@PathVariable("studentId") Integer studentId){
+    public Student getStudent(@PathVariable("studentId") Integer studentId,HttpServletRequest requestt){
 
-
-        return StudentsList.stream().
+        System.out.println("Your Session Id = " + requestt.getSession().getId());
+        return studentsList.stream().
                 filter(stu -> studentId.equals(stu.getStudentId())).
                 findFirst().
-                orElseThrow(()-> new IllegalArgumentException("studen "+studentId+" does not exist"));
+                orElseThrow(()-> new IllegalArgumentException("studen "+studentId+" does not exist" +"Your Session Id = " + requestt.getSession().getId() ));
     }
+
+
+   @GetMapping(path = "list")
+   public List<Student> getList(HttpServletRequest requestt){
+       System.out.println("Your Session Id = " + requestt.getSession().getId());
+        return studentsList;
+   }
+
+
+    @GetMapping(path = "csrf")
+    public CsrfToken getCsrf(HttpServletRequest requestt){
+        return (CsrfToken) requestt.getAttribute("_csrf");
+    }
+
+
+
+    @PostMapping(path = "list")
+    public Student addToList(HttpServletRequest requestt,@RequestBody Student student){
+
+        System.out.println("Your Session Id = " + requestt.getSession().getId());
+        studentsList.add(student);
+        return student;
+    }
+
+
 
 
 }
